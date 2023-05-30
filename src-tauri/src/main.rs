9d47f1c4ed61c95589d1fb1d4bc1fe7891642754
Max Grupper <m.grupper@adguard.com>
2023-05-30 21:15:14 +0300

    use transparent icon only on mac

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -37,8 +37,12 @@ async fn main() {
         });
     }
     trace!("Create tray window");
-    let system_tray = SystemTray::new().with_menu(tray::system_tray_menu());
-    let app = tauri::Builder::default()
+    let mut system_tray = SystemTray::new().with_menu(tray::system_tray_menu());
+    #[cfg(target_os = "macos")] 
+    {
+        system_tray = system_tray.with_icon(tauri::Icon::Raw(include_bytes!("../icons/tray_icon.png").to_vec()));
+    }
+    let mut app = tauri::Builder::default()
         .plugin(log.build())
         .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_autostart::init(
