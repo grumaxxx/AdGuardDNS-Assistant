eda548b5199f5779071532bb01c4be8674737ac6
Max Grupper <m.grupper@adguard.com>
2023-05-31 22:33:29 +0300

    fix format

--- src/components/Tray/StatisticCard.tsx
+++ src/components/Tray/StatisticCard.tsx
@@ -10,12 +10,14 @@ interface StatisticCardProps {
 function formatNumber(num: number): string {
   if (num < 1000) {
     return num.toFixed(0);
-  } else if (num < 100000) {
+  } else if (num < 10000) {
     return (num / 1000).toFixed(1) + 'K';
-  } else if (num < 1000000) {
+  } else if (num < 995000) {
     return Math.round(num / 1000) + 'K';
-  } else {
+  } else if (num < 10000000) {
     return (num / 1000000).toFixed(1) + 'M';
+  } else {
+    return Math.round(num / 1000000) + 'M';
   }
 }
 
